e69a6149f0dafef35fcae97b5ac75440e9f195e7
kmichalczyk <k.michalczyk@radwag.pl>
2019-10-31 14:59:55 +0100

    zmiany w pliku callback.js

--- node/callback.js
+++ node/callback.js
@@ -1,1 +1,12 @@
-const add = (x, y) => { return x + y}
+const add = (x, y) => { return x + y};
+const division = (number1, number2) => number1 / number2;
+
+const math = (a, b, callback) => {
+    console.log(callback(a, b))
+};
+
+math(4, 2, add);
+math(4, 2, division);
+
+
+setTimeout( () => console.log("Hejka") ,2000)
