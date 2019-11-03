237e2e1cf66e981a32996c235b5bcf59c2b5d8dd
Karol Michalczyk <keszua@gmail.com>
2019-11-03 20:50:20 +0100

    Trening na modulach

---
+++ mod/app.js
@@ -0,0 +1,5 @@
+
+const notes = require('./notes')
+
+console.log(notes.txt)
+console.log("Jetem w module app.js")
---
+++ mod/notes.js
@@ -0,0 +1,5 @@
+console.log("Jetem w module notes.js")
+
+module.exports = {
+  txt:  "cos zwrócone z modułu notes"
+}
