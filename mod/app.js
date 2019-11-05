1ae14c16e615f2e8be50fbc8706b0fbc2e4948ad
Karol Michalczyk <keszua@gmail.com>
2019-11-05 21:49:21 +0100

    Przerobione filmiki 28 i 29. Drobne zmiany sciagi z Tablice.js

--- mod/app.js
+++ mod/app.js
@@ -1,5 +1,19 @@
 
-const notes = require('./notes')
+const notes = require('./notes');
+const counter = require('./counter');
 
-console.log(notes.txt)
-console.log("Jetem w module app.js")
+//console.log(notes.txt)
+//console.log("Jetem w module app.js");
+
+
+// counter.add(3);
+// counter();
+// counter();
+// counter();
+// counter.add(3);
+
+const users = require('./users');
+
+users.showUsers();
+users.showUserObj(4);
+console.log(users.userListlenght);
