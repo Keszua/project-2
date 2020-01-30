0df4bcd73e8b40e8bfdf54829fad8159e0f21a18
kmichalczyk <k.michalczyk@radwag.pl>
2020-01-30 14:21:55 +0100

    Dodany folder SVG

--- Sciagi/Grafika_i_animacje.js
+++ Sciagi/Grafika_i_animacje.js
@@ -10,7 +10,7 @@ Jako źródo wiedz o canvasie: mdn?
 //Ciekawa stronka z o Canvas:
  https://developer.mozilla.org/pl/docs/Web/API/Canvas_API/Tutorial/Optymalizacja_canvas
  
-// Kurs canvy, jak naryswać kartezjański ukłąd współżędnych:
+// Kurs canvy, jak naryswać kartezjański układ współżędnych:
 http://shebang.pl/kursy/html5-rzeczowo/r5-canvas/ 
 
 //-----------------------------------------------------------------------------
@@ -85,16 +85,97 @@ const rotate5 = (currentTime) => {
 }
 rotate5(); 
 //-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+SVG
+//   ###   #   #   ###              
+//  #   #  #   #  #   #           
+//  #      #   #  #            
+//   ###   #   #  #  ##          
+//      #  #   #  #   #           
+//  #   #   # #   #   #       
+//   ###     #     ###         
 
 
+Przed rozpoczęciem pracy w Visual Studio Code, proponowany jest doatek "SVG"  (SVG Language Support)
+Dodatek ten podpowiada składnie i tworzy okienko z podglądem (PM na plik .svg i "Preview SVG" ) 
+
+INKSCAPE - Jakiś darmowy program do tworzenia grafiki SVG
+
+
+
+
+//    SVG skałda sie z 3 części:
+//    1. Canvas SVG - czyli "cała mapa poziomu"
+//    2. element SVG
+//    3. viewBox - czyli obszar widoczny na ekranie
+
+//Dodawanie SVG jako "zwykły" obrazek: (w pliku .html)
+	<img src="img/sun.svg" alt="słońce">
+
+//Dodawnaie SVG jako obrazek w wersji edytowalnej (w pliku .html)
+    <!-- Trzeba doać atrybut xmlns, aby korzystać z SVG w wersji edytowalnej -->
+    <svg id="sun" xmlns="http://www.w3.org/2000/svg"
+        width = "600" height="600" viewBox="0 0 600 600">  
+        <!-- Kaształty: -->
+        <rect class="clips" x="150" y="250" width="100" height="20" fill="white" stroke="red" stroke-width="8" />
+        <!-- Domyślnie jedneostki sa w px. Pozycja x i y domyślnie jest 0 0. Stroke to obramowanie. -->
+        <circle cx="200" cy="200" r="20" fill="#333" stroke="black" stroke-width="8" />
+        <ellipse cx="300" cy="100" rx="50" ry="70" fill="#eee" stroke="rgba(200, 200, 100, 0.5)" stroke-width="4" />
+        
+        <!-- Linia
+        Polygon (wielokąt)
+        Polyline (linia łamana) -->
+        <polyline points="10,40 50,40 30,200" fill="white" stroke="yellow" stroke-width="8" />
+
+        
+        <path d="M200 200 L350 200 L275 250 Z" fill="white" stroke="yellow" stroke-width="8" />
+        <!--  d - ścieżka M- moveTo  L - lineTo  Z - closepatch (łączy pierwszy z ostatnim) -->
+
+        <!-- Grupowanie -->
+        <g id="face" fill="white">
+            <circle cx="150" cy="150" r="60" fill="#333" stroke="black" stroke-width="8" />
+            <rect class="clips" x="100" y="100" width="100" height="100" fill="none" stroke="blue" stroke-width="8" />
+        </g>
+
+    </svg>
+
+//możliwość edycji parametrów przez CSS (nie polecane)
+// można stylować grubośc linii itp. ale nie można edytowac współżędnych
+	<style>
+		#Obrazki .clips {
+			fill: red;
+		}
+    </style>
+
+//Edycja parametrów za pomocą GSock
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js" ></script>
+    <script>
+        TweenMax.set('.clips', {fill: "red"})
+        TweenMax.to('.clips', .3, {height: "+=40", repeat: -1, yoyo: true, fill: "yellow"}) 
+    </script>
+
+
+</body>
+</html>
 
 
 //-----------------------------------------------------------------------------
+//Stronka, z efektami do SVG
+https://www.w3schools.com/graphics/svg_grad_radial.asp
+
 
 //-----------------------------------------------------------------------------
-
 //-----------------------------------------------------------------------------
-Efekt dyskotekowy (podejżany na stronei Radwag)
+//-----------------------------------------------------------------------------
+Programy online do rysowanie SVG:
+https://editor.method.ac/#copy
+http://www.clker.com/inc/svgedit/svg-editor.html
+
+Przykład robota ruszającego nozkami, wykorzystanei "TimelineMax()" w filmie 275 (Zaawansowane projekty w CSS i JavaScript)
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+//Efekt dyskotekowy (podejżany na stronei Radwag)
 
 //----- html: 
 <div class="wag1_promienie">
