4cfddf6fdfe98ca8ef02fd639f1a4611388cca58
kmichalczyk <keszua@gmail.com>
2020-03-12 13:40:18 +0100

    Dodaj python

---
+++ Sciagi/python.py
@@ -0,0 +1,53 @@
+
+
+srodowisko mozna ściagnąc z https://www.python.org/  zakałdka Download.
+Koles poleca zaznaczenie "Add python 3.8 to PATH" pozwoli to na dostęp z CMD
+
+proponowane IDE: PyCharm - wersje darmową Community 
+
+1. Tworzymy projekt. Po stworzeniu projektu, folder jest pusty.
+2. Tworzymy plik run.py
+3. Wpisujemy kod, np: print(Witaj")
+4. Dodajemy konfigurację "add Configuration"
+	Plusikiem -> wybieramy "Python" -> 
+		-> Wybeiramy skrypt, czyli nasz "run.py" -> OK
+	Reszta domyślna. 
+5. Powinna nam się aktywować zielona strzałaka do uruchomienia.
+6. Wynik zobaczymy w konsoli
+
+//-----------------------------------------------------------------------------
+
+print(type(x))	//wypisze typ zmiennej
+x = input() // czeka na wpisanie danej w konsoli (zawsze traktuje to jako ciąg znaków)
+x = int(x) 	//konwersja na int
+x = float(x) 	//konwersja na float  10.5
+
+
+x = "Ala ma kota a kot ma Alę :)"
+print(x[0])		//= A
+print(x[-1])	//= )						    -1 to ostatni znak
+print(x[0:3])	//= Ala  od którego znaku do którego (gdzie 3-ciego już nie drukuje)
+print(x[5:])	//= a kota a kot ma Alę :)    	od 5 do końca
+print(x[:5])	//= Ala m						od 0 do 5-tego 
+print(x[-5:])	//= lę :)						-1 to ostatni znak
+
+#-----------------------------------------------------------------------------
+x = 10
+if x == 10:
+    print("Warunek spelniony")
+else:
+    print("Nie zgadza sie")
+
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+#kurs RestAPIPython
+W kursie korzyta z narzedzi:
+
+docker-compose
+
+
+#-----------------------------------------------------------------------------
+
+#-----------------------------------------------------------------------------
+
---
+++ aiohttp/hello.py
@@ -0,0 +1,5 @@
+#Uruchamia sie zieloną strzałką
+#Filmik w RestAPIPython - ale za ciężki temat dla mnie
+print("Działa")
+
+
