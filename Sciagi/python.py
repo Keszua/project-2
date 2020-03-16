30d828c02a016a3fd2cb2cdced516c7d7292a4d7
kmichalczyk <keszua@gmail.com>
2020-03-16 16:06:15 +0100

    Odcinek 15 python

--- Sciagi/python.py
+++ Sciagi/python.py
@@ -26,17 +26,21 @@ type(x)			# sprawdzamy jaki to typ
 x = "Ala ma kota a kot ma Alę :)"
 print(x[0])		//= A
 print(x[-1])	//= )						    -1 to ostatni znak
-print(x[0:3])	//= Ala  od którego znaku do którego (gdzie 3-ciego już nie drukuje)
+print(x[0:3])	//= Ala  	od którego znaku do którego (gdzie 3-ciego już nie drukuje)
 print(x[5:])	//= a kota a kot ma Alę :)    	od 5 do końca
 print(x[:5])	//= Ala m						od 0 do 5-tego 
 print(x[-5:])	//= lę :)						-1 to ostatni znak
 
-#-----------------------------------------------------------------------------
-x = 10
-if x == 10:
-    print("Warunek spelniony")
-else:
-    print("Nie zgadza sie")
+.format  #doklejanie elemenów w miejsce "klamerek"
+print("Tekst bazowy + {} {}".format("dolejony1", 7)) #=  Tekst bazowy + dolejony1 7
+# to samo co wyżej:
+x = "Tekst bazowy  + {} {}"
+y = x.format("dolejony1", 7)
+print(y)		#= Tekst bazowy  + dolejony1 7
+
+print("Moja funkcja", end='')	# aby nie przenosić na następną linię
+print(" dalszy tekst")
+
 
 #-----------------------------------------------------------------------------
 #lista
@@ -52,10 +56,145 @@ x = produkty.index("mleko")	#=0  zwróci pozycje podanego elementu
 #tuple - nie edytowalna lista
 produkty = ("mleko", "ser", "parówki")
 
-#słowniki -  analogicznie do obiektu w JS albo json
+#dict (słowniki) -  analogicznie do obiektu w JS albo json
 person = {  "wiek" : 20, "imie" : "Anna" }
 print(person)           # wypisze cały obiekt
 print(person["imie"])   #= Anna
+print(person.get("wiek", 5) ) #= zwróci 20 (gdy znajdzie) lub 5 gdy nie znajdzie
+
+
+#-----------------------------------------------------------------------------
+#Operatory logiczne:
+and  	# oraz, czyli &&
+or		# lub,  czyli ||
+not 	# negacja, np:  if not 5 in lista : print("Lista nie zawiera elementu")
+
+
+#-----------------------------------------------------------------------------
+x = 10
+if x == 10:
+    print("Warunek spelniony")
+else:
+    print("Nie zgadza sie")
+
+
+if "mleko" in produkty:		# czy coś jest w danej liście. Trochę jak:  produkty.index("mleko") 
+    print("Znaleziono")
+else:
+    print("Nie znaleziono")
+
+
+if "jajka" in produkty:
+    print("Znaleziono jajka")
+elif "ser" in produkty:				# else if
+    print("Znaleziono ser")
+elif True:							# else lub elif True
+    print("Nie znaleziono")
+
+
+
+#-----------------------------------------------------------------------------
+#wypisze cyfry od 0 do 10:
+i = 0
+while i <= 10:
+    print(i)
+    i += 1 
+
+#wypisze wszystkie elementy lity 
+for el in produkty:
+    print(el)
+
+# podobnie jak to wyżej, ale wypisze to za pomocą automatycznego wstawiania za pomoca .format
+for i, produkt in enumerate(produkty):
+    print("Produkt: {}, o indeksie: {}".format(produkt, i) )
+
+
+# generator:  
+for el in range(10):		# wygeneruje 10 elementów
+    print(el)
+
+for el in range(10, 20):	# wygeneruje liste od 10 do 19
+    print(el)
+
+for el in range(10, 20, 2): # wygeneruje liste: 10, 12, 14, 16, 18
+    print(el)
+
+for i, produkt in enumerate(produkty):
+    if i == 2: continue 	# continue: pomija jeden elenet. break - kończy pętle
+    print(i)				# zwróci numer, czyli od 0
+    print(produkt)			# zwróci elementy listy, czyli od "mleko"
+
+
+#-----------------------------------------------------------------------------
+funkcje
+
+def printme():
+    print("moja funkcja")
+printme()	# wywołanie mojej funkcji
+
+
+def odejmij(a, b):
+    return a - b
+print(odejmij(4, 2))		#= 2
+print(odejmij(b=4, a=2))	#= -2 		mogę podawać argumenty nie pokolei
+
+
+
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+#moduły zewnętrzne (odział programu na pliki)
+
+#treść pliku "modul.py"  (nazwa ma znaczenie)
+def test(tekst):
+    print("Tekst z pliku zewnętrznego:", tekst )
+
+#treśc pliku "main.py"
+import modul
+modul.test("elo")		#= Tekst z pliku zewnętrznego: elo
+
+#drugi sposób korzystania z modułu zewnętrznego z wyłuskaniem funcji z danego pliku:
+from modul import test
+test("elo")				#= Tekst z pliku zewnętrznego: elo
+
+#można wszystko zaimportować:
+from modul import *
+
+#gdy chce importować funkcje i zmienić jej nazwę:
+from modul import test as test_external
+test_external("elo") 	#= Tekst z pliku zewnętrznego: elo
+
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+# biblioteka time
+import time
+
+time.sleep(5)   # zawiesza program na 5 sekund
+time.time()		# aktualny czas ( w sekundach )
+
+
+# przykładowy program, który wypisze 5 razy napis i wyjdzie z petli:
+timer1 = time.time()
+timer5 = time.time()
+while True:
+    if time.time() - timer1 > 1:
+        timer1 = time.time()
+        print("1 sek")
+    if time.time() - timer5 > 5: break
+        
+ 
+#-----------------------------------------------------------------------------
+# biblioteka datetime
+teraz = datetime.datetime.now()
+print(teraz)	#= 2020-03-16 14:24:38.447862
+print(str(teraz.hour)+":"+str(teraz.minute)+":"+str(teraz.second))	#= 14:24:38
+print(teraz.strftime("%H:%M:%S"))									#= 14:24:38
+print(teraz.strftime("%d.%m.%y"))									#= 16.03.20
+%I - godzina w systemie 12 godzinnym
+%p - wypisze AM albo PM
+%b - miesiąc skrucona nazwa
+%B - miesiąc pełna nazwa
 
 
 
---
+++ python/main.py
@@ -0,0 +1,20 @@
+#Uruchamia sie zieloną strzałką
+
+#import datetime
+
+
+print("--- Start ---")
+
+
+# f = open("plik.txt", mode="a+")     # otwarcie pliku, w modzie otwórz lub stwórz i otwórz jeśli go nie ma
+# f.write("Ddoany tekst ")            # wpisanie tekstu
+# f.close()                           # zamknij plik
+
+f = open("plik.txt", mode="r")        # tyko odczyt
+x= f.read()
+f.close()
+print(x)
+
+skończyłem na połowie filmiku 15
+
+print(" . - End - . ")
