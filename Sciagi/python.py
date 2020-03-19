da4793e7bc25eae4f341bffcc0ceb09b23c22037
kmichalczyk <keszua@gmail.com>
2020-03-19 16:00:23 +0100

    Prosta gra w Pyton

--- Sciagi/python.py
+++ Sciagi/python.py
@@ -7,7 +7,7 @@ proponowane IDE: PyCharm - wersje darmową Community
 
 1. Tworzymy projekt. Po stworzeniu projektu, folder jest pusty.
 2. Tworzymy plik run.py
-3. Wpisujemy kod, np: print(Witaj")
+3. Wpisujemy kod, np: print("Witaj")
 4. Dodajemy konfigurację "add Configuration"
 	Plusikiem -> wybieramy "Python" -> 
 		-> Wybeiramy skrypt, czyli nasz "run.py" -> OK
@@ -254,7 +254,7 @@ os.makedirs(dir_path)					# tworzy same foldery
 open(path, "w").close()					# torzy i zamyka plik
 
 #-----------------------------------------------------------------------------
-wyjątki
+Wyjątki
 
 #proba otwarcia nieistniejącego pliku
 try:
@@ -279,17 +279,49 @@ except Exception as e:
     print(e)
 
 
+# Właśny wyjątek w swojej funkcji:
+# Etam 1:
+class TooColdException(Exception):
+    pass
+
+def celcius_to_kelvin(temp):
+    if temp < -273.15:
+        raise TooColdException()     #wywołanie wyjątku
+    return temp + 273.15
+
+try:
+    print(celcius_to_kelvin(-300))
+except TooColdException:
+    print("Zbyt zimno! - sprawdz czujnik")
+
+# Etam 2:
+class TooColdException(Exception):
+    def __init__(self, temp):
+        super().__init__("Zbyt zimno! Temp= {} Sprawdz czujnik!".format(temp))
+
+def celcius_to_kelvin(temp):
+    if temp < -273.15:
+        raise TooColdException(temp)     #wywołanie wyjątku
+    return temp + 273.15
+
+print(celcius_to_kelvin(-300))
+
 
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 #-----------------------------------------------------------------------------
 Klasy
 
+Pusta klasa:
+class TooColdException(Exception):
+    pass
+
 class Calculator():
-    def __init__(self):
+    def __init__(self): 	# konstruktor
+		self.ostatni_wynik = 0
         print("Init calc")
 		self.liczba = 10
-    def __del__(sef):
+    def __del__(sef): 		# destruktor
         print("Del calc")
     def __str__(self):  	# wykonuje się, gdy wywołamy print(str(obiekt))
         return "Metoda str"
@@ -300,14 +332,118 @@ class Calculator():
 
     def dodaj(self, a, b):
         wynik = a + b
+		self.ostatni_wynik = wynik
         print("Wynik dodawania: ", wynik)
     def odejmij(self, a, b):
         wynik = a - b
+		self.ostatni_wynik = wynik
         print("Wnik odejmowania: ", wynik)
 
 calc = Calculator() 	# tworzenie instancji
 del calc 				# ręczne usówanie obiektu (po zakończeniu programu, albo po wyjściu
 						# z zakresu funkcji w której były stworzone, obiekty same się usuwają)
+
+
+Dziedziczenie
+class Person():
+    def __init__(self, name = "Person: undefined name", surname = "Person: undefined surname"):
+        self.name = name
+        self.surname = surname
+
+class Employee(Person):
+    def __init__(self, name = "name = ?", surname = "surname = ?", positon = "Undefined positon" ):
+        super().__init__(name, surname)
+        self.positon = positon
+
+class Client(Person):
+    def __init__(self, ordered = "Website"):
+        super().__init__()
+        self.ordered = ordered
+
+p = Person("Krzysiek", "Nowak") 
+print(p.name, p.surname)           	#= Krzysiek Nowak
+
+em = Employee("Tomek", positon="Programista")
+print(em.name, em.positon)  		#= Tomek Programista
+
+cl = Client()
+print("Name: {}, Ordered: {}".format(cl.name, cl.ordered)) #= Name: Person: undefined name, ordered: Website
+
+
+
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+PyGame na postawie filmiku: https://www.youtube.com/watch?v=tnq0whNwhZE&t=159s
+
+Trzeba zainstalować PyGame:
+W PyCharm trzeba wejść: 
+File -> Settings - >	(otworzy się okienko)
+-> Project: twojaNazwaprojektu ->  project Interpreter -> klikamy na "+" ->
+-> W wyszukiwaniu wpisujemy: "pygame" -> Install Package
+Jeżeli nie mamy dostepu admnistratora, to trzeba to zrobić przez konsolę:
+	pip install pygame
+
+Jeżeli nadal nei mamy dostępu, to PM na mój koputer -> Właściwości -> Zaawansowane ustawienia systemu ->
+-> Zaawqnsowane -> Zmienne środowiskowe... -> Path -> Edytuj -> Nowy ->
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\Scripts\
+C:\Users\user\AppData\Local\Programs\Python\Python38-32\
+Teraz wyszukaj programu IDLE -> uruchomi się konsola
+W konsoli jak się wpisze:
+	import pygame
+i nie będzie żadnego komunikatu, to znaczy że udało się zaistalować.
+
+#-----------------------------------------------------------------------------
+# Aby sprawdzić czy w programie działa, trzeba wywołać 2 linijki:
+import pygame
+print(pygame.__version__)
+
+#-----------------------------------------------------------------------------
+# Najprostrzy program rysujący prostokąt:
+import pygame, sys
+
+screen = pygame.display.set_mode((1280,720))
+
+while True:
+	# Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+    # Drawing
+					#uchwyt, kolor,        kształt
+    pygame.draw.rect(screen, (0, 150, 250), pygame.Rect(10, 50, 200, 100))
+    pygame.display.flip()
+#-----------------------------------------------------------------------------
+# Program z rysowaneim i przemieszczeniem prostokątu:
+import pygame, sys
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+box = pygame.Rect(10, 50, 50, 50)
+
+while True:
+    # Handle events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            print("Space")    
+
+    # Checking inputs
+    if pygame.key.get_pressed() [pygame.K_d]:
+        box.x +=1
+    keys = pygame.key.get_pressed()
+    if keys [pygame.K_a]:
+        box.x -=1
+    if keys [pygame.K_s]:
+        box.y +=1
+    if keys [pygame.K_w]:
+        box.y -=1
+
+    # Drawing
+    screen.fill((0, 0, 0))
+    pygame.draw.rect(screen, (0, 150, 255), box)
+    pygame.display.flip()
+#-----------------------------------------------------------------------------
 
 
 
--- python/main.py
+++ python/main.py
@@ -1,47 +1,61 @@
 #Uruchamia sie zieloną strzałką
 
-#import datetime
+import pygame, sys
+from rocket import Rocket
 
-class Calculator():
+
+class Game(object):
+
+    def __init__(self):
+        # Config
+        self.tps_max = 25.0
+
+        #Initialization
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        icon = pygame.image.load("rocket.png")
+        pygame.display.set_caption("Space rocket")
+        pygame.display.set_icon(icon)
+        
+        self.tps_clock = pygame.time.Clock()
+        self.tps_delta = 0.0
+
+        self.player = Rocket(self)
+
+        while True:
+            # Handle events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    sys.exit(0)
+
+            # Ticking
+            self.tps_delta += self.tps_clock.tick() / 1000   # dt - to czas ms miedzy klatkami
+            while self.tps_delta > 1/self.tps_max:
+                self.tick()
+                self.tps_delta -= 1/self.tps_max
+
+            # Drawing
+            self.screen.fill((0, 0, 0))
+            self.draw()    
+            pygame.display.flip()
+
+    def tick(self):
+        self.player.tick()
+        # Checking inputs
+        keys = pygame.key.get_pressed()
+
+
+    def draw(self):
+        # Drawing
+        self.player.draw()
+        
+        
+if __name__ == "__main__":
+    Game()
+
+
+
     
-    def __init__(self): # konstruktor
-        print("Init calc")
-        self.liczba = 10
 
-    def __del__(sef):   # destruktor
-        print("Del calc")
-
-    def __str__(self):  # wykonuje się, gdy wywołamy print(str(obiekt))
-        return "Metoda str"
-
-    def __int__(self):  # wykonuje się, gdy wywołamy print(int(obiekt))
-        return 10
-
-    def __len__(self):  # wykonuje się, gdy wywołamy print(len(obiekt))
-        return 5
-
-
-
-    def dodaj(self, a, b):
-        wynik = a + b
-        print("Wynik dodawania: ", wynik)
-
-    def odejmij(self, a, b):
-        wynik = a - b
-        print("Wnik odejmowania: ", wynik)
-
-
-
-calc = Calculator() 
-calc.dodaj(2,3)
-
-# calc.liczba += 5
-
-print(calc.liczba)
-
-
-# print("--- Start ---")
-
-# skończyłem na  filmiku 20
-
-# print(" --- End --- ")
