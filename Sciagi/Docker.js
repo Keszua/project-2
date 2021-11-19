55446967acc5cadb59d6207609caf1ba6a32e11e
Karol_Michalczyk <k.michalczyk@humansoft.pl>
2021-11-19 16:04:15 +0100

    aktualiuj dockera

--- Sciagi/Docker.js
+++ Sciagi/Docker.js
@@ -8,6 +8,9 @@ https://docs.microsoft.com/pl-pl/windows/wsl/install-manual#step-4---download-th
 // zak³adanie konta na:
 https://hub.docker.com/signup
 
+
+// link do repo z kursu: 
+https://github.com/pnowy/docker-course
 //-------------------------------------------------------------------------------------------------
 docker version
 docker info       // liczba kontenerów i obraów
@@ -17,9 +20,10 @@ docker-compose    // jakieœ narzêdzie napisane w Pythonie
 
 docker            // wyœwietli listê poleceñ
 
-
+docker image pull nazwaObrau  // œci¹gnieci danego obrazu, bez uruchamiania
+docker image pull nazwaObrau:mojTag  // œci¹gnieci danego obrazu + nadaj tag ale tez jakoœ koleœ œciaga³ odpowiednie wersje za pomoc¹ tagu (film 18)
 docker run
-docker container run   // uruchomienie NOWEGO kontenera
+docker container run   // uruchomienie NOWEGO kontenera. Je¿eli nie posiadamy obrazu, zostanei on œci¹gniêty i uruchomiony
 docker container run --publish 8080:80 --detach nginx  
 //                     |       |    |    |      |- nazwa obrazu
 //                     |       |    |    |- uruchomienie kontenera w tle
@@ -35,7 +39,7 @@ docker container run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -
 //                                              |- przekazanie zmiennej œrodowiskowej
 
 // uruchom w trybie interaktywnym z terminalem (wyjœcie komend¹ exit)
-docker container run -it nginx bash
+docker container run -it nginx bash 
 docker container start -ai idkontenera
 docker container exec -it idkontenera bash  //odpalenie konsoli na dzia³aj¹cym kontenerze
 
@@ -55,6 +59,7 @@ docker container logs nazwaKontenera -f //ci¹g³a obserwacja (zablokuje CLI)
 curl localhost:8080  //zwróci kod HTML strony
 curl localhost:9200  //zwróci JSON (jeœli mamy odpalony elastic)
 
+//curl - taka rzegl¹darka z linii komend
 curl -H "Content-Type: application/json" -XPOST "http://localhost:9200/docker/image/1" -d "{ \"name\" : \"elasticsearch\"}"
 curl localhost:9200/docker/_search
 
@@ -64,7 +69,7 @@ docker run -d --name rabbit-in-the-hole -p 8081:15672 rabbitmq:3-managment
 
 
 docker container top idkontenera      // obserwacja procesów
-docker container inspect idkontenera  // wyswietla pe³n¹ konfiguracjê kontenerza (w formacie JSON)
+docker container inspect idkontenera  // wyswietla pe³n¹ konfiguracjê kontenera (w formacie JSON)
 docker container stats                // "live striming" ogólny status wszystkich uruchominych kontenerów
 
 
@@ -80,14 +85,63 @@ docker container run -d --network=skynet nginx
 docker network connect skynet first   // pod³¹czenie do sieci, kontenera first
 docker network disconnect bridge first // odpiêcie kontenera od domyœlnej sieci (bo teraz by³y dwie sieci)
 
-//Usówanie sieci:
+//Usuwanie sieci:
 // w pierszej kolejnosci trzeba odpi¹æ lub usun¹æ kontenery
+
+// DNS
+// Film 17 (Docker od podstaw)
+// Trzeba sprawdziæ IPAddress  (polecenie docker network inspect ...)
+// Endpoint informujacy o stanie serwisu: (w konsoli linuxa)
+curl http://172.17.0.3:8080/actuator/health
+
+
+// urzycie opcji link ... na przyk³adzie po³¹czenia ado bazy MSQL
+docker run --name wordpressdb -e MYSQL_ROOT_PASSWORD=wordpress -e MYSQL_DATABASE=wordpress -d mysql:5.7
+docker run -e WORDPRESS_DB_PASSWORD=wordpress -d --name wordpress --link wordpressdb:mysql -p 80:80  wordpress:5-php7.2
+//                                  |                   |                |                    |- port zewnêtrzny dla przegl¹darki
+//                                  |                   |                |           |- wewnêtrzny alias, domyœlnia nazwa dla bazy danych to mysql
+//                                  |                   |                |- nazwa kontenera
+//                                  |                   |- nazwa kontenera
+//                                  |- nazwa bazy danych
+
+
+//-------------------------------------------------------------------------------------------------
+// Budowanie obrazów
+
+docker image history nazwaObrazu   // mozna podej¿eæ, kiedy i z jakich warstwy zosta³ stworzony obraz
+docker image inspect idKontenera  // wyswietla pe³n¹ konfiguracjê obrzu (w formacie JSON)
+
+// nadawanie nowego tagu
+docker image tag nazwa:aTagiem nazwaKontaDockerHub/nazwa
+//                                                  |- nazwa z ewentualnym tagiem
+
+//wys³anie obrazu na w³asne repozytorium:
+// trzeba byæ zalogowanym, te same dane co na https://hub.docker.com/ :
+docker login 
+docker image push nazwaKontaDockerHub/nazwaObrazu:zTagiem
+docker logout // jeœli chcemy siê wylogowaæ 
+
+//pobranie ze swojego repo
+docker image pull nazwaKontaDockerHub/nazwaObrazu:zEwentualnymTagiem
+
+// tworzenie obrazu na podstawie kontenera:
+docker container commit idKontenera nazwaKontaDockerHub/nazwaObrazu:zTagiem
+
+// tworzenie kontenera na z wykorzsytaneim pliku Dockerfile
+// w filmie 21 koleœ pokazuje plik Dockerfile gdzie definiuje siê, jak przerobiæ i skonfigurowaæ obraz
+https://docs.docker.com/engine/reference/builder/
+// trzeba byæ konsol¹ w folderze w którym jest plik Dockerfile
+docker image build -t nowaNazwaObrazu:tag .  //nie jestem pewien co do nazw obrazów
+
+
+
+
 
 
 //poczytac o Kubernets
 
 
-
+https://ihermes.humansoft.pl/login
 
 
 
