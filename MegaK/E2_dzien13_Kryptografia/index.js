2170f41c953f42ae7981a343abc0ef508fb9c4c7
Karol M <keszua@gmail.com>
2022-05-29 23:25:55 +0200

    kryptografia

--- MegaK/E2_dzien13_Kryptografia/index.js
+++ MegaK/E2_dzien13_Kryptografia/index.js
@@ -1,7 +1,7 @@
 // Dzień 13 Kryptografia
 const {	ecryptText,	decryptText } = require('./cipher');
 
-const SALT = 'diJIJI';
+const SALT = 'kadjfkjakfj@KJK#$';
 
 (async () => {
 	const encrypted = await ecryptText('Heja banana', 'tajne haslo', SALT);
@@ -11,3 +11,41 @@ const SALT = 'diJIJI';
 	const decryptred = await decryptText(encrypted.encrypted, 'tajne haslo', SALT, encrypted.iv);
 	console.log(decryptred);
 })();
+
+
+
+// dzień 15
+// bezpieczne hashowanie 1
+const {createHmac} = require('crypto');
+
+const hash = createHmac('sha512', SALT)
+    .update('Tekst do zahashowania')
+    .digest('hex');
+
+console.log(hash);
+
+
+// bezpieczne hashowanie 2
+const { pbkdf2 } = require('crypto');
+
+const oryginalText = 'Hello, World!';
+
+pbkdf2(oryginalText, SALT, 100000, 64, 'sha512', (err, derivedKey) => {
+
+    if (err) throw err;
+    console.log(derivedKey.toString('hex'));
+
+});
+
+
+// bezpieczne hashowanie 3
+// bcrypt, nalezy zainstlawoac bcrypta  npm i bcrypt
+const {hash} = require('bcrypt');
+
+hash('tekst do zhasowania', 10, (err, hash) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(hash);
+    }
+});
